50f2041414110cddb1f4480ba533688bd1a29367
Amitkumar Singh <amit.bh.singh@gmail.com>
2018-05-27 11:03:26 -0400

    Update winequality.R
    description of the dataset added

--- winequality.R
+++ winequality.R
@@ -1,10 +1,16 @@
-#Assignment 03 
-#Course 642
+# Analysing the fo
+#Data Set Information:
+
+#The two datasets are related to red and white variants of the Portuguese "Vinho Verde" wine. For more details, consult: [Web Link] or the reference [Cortez et al., 2009]. Due to privacy and logistic issues, only physicochemical (inputs) and sensory (the output) variables are available (e.g. there is no data about grape types, wine brand, wine selling price, etc.). 
+
+#These datasets can be viewed as classification or regression tasks. The classes are ordered and not balanced (e.g. there are munch more normal wines than excellent or poor ones). Outlier detection algorithms could be used to detect the few excellent or poor wines. Also, we are not sure if all input variables are relevant. So it could be interesting to test feature selection methods. 
+
+# installing the packages required 
 install.packages("class")
 install.packages("gmodels")
 library(class)
 library(gmodels)
-
+# importing data 
 dataurl<-"http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
 wine<-read.csv(dataurl,header = TRUE,sep = ";")
 str(wine)
@@ -17,7 +23,7 @@ View(wine)
 which(is.na(wine))
 boxplot(wine)
 
-
+#normalizing data 
 normalize <- function(x) {
    return ((x - min(x)) / (max(x) - min(x))) }
 winen <- as.data.frame(lapply(wine[1:11], normalize))
